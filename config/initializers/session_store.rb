b0b02321534ec93aab46b3abe0bc033c3ea0a907
Dave Rosborough <dave@rosboroughtech.com>
2014-03-17 22:54:15 -0700

    Removed ref to encrypted_cookie_store from beta initializer

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Drosophila::Application.config.session_store :encrypted_cookie_store, key: '_drosophila_session'
+Drosophila::Application.config.session_store :cookie_store, key: '_drosophila_session'
