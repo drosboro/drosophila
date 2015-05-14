08b1aaffc814fcf3206a95cd8c9f0ae8ef6c972f
Dave Rosborough <dave@rosboroughtech.com>
2015-05-13 19:37:34 -0700

    Added relative_url_root to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Drosophila::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.relative_url_root = "/drosophila"
 end
